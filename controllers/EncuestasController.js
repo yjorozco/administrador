02ee184b0943100af73d7187f55f34399aa71c84
Yamil Orozco <yamiloro@gmail.com>
2021-03-10 00:31:48 -0430

    agregando cosas

--- controllers/EncuestasController.js
+++ controllers/EncuestasController.js
@@ -12,64 +12,68 @@ exports.procesarEncuesta = async (req, res, next) => {
     let usuario = await req.user;
 
     const intensidades = Object.values(respuestas);
-    console.log(intensidades);
+
     try {
         if (!errors.isEmpty()) {
             console.log(errors);
             next(new HttpError('Datos invalidos', 422));
         }
 
-        const intensidadesSuma= await db.Intensidades.findAll({
+        const intensidadesObject = await db.Intensidades.findAll({
+            atributes: ['id', 'nombre','valor'],
             where: {
-                id: {[Op.in]: intensidades}
-            }
-
+                id: { [Op.in]: intensidades }
+            }        
         })
 
-        console.log(intensidadesSuma);
-       /* const numero = await respuestas.length;
-        const intensidadValor = Math.round(intensidadesSuma/numero);
-        const intensidad = await db.Intensidades.findAll(
-            { where: { 'valor': intensidadValor } }
-        )
-        let fechaSismo = await fecha + ' ' + hora;
-        console.log(intensidad);
-        fechaSismo = dateFormat(fechaSismo, "yyyy-mm-dd h:MM:ss");
-        const encuestas = await db.Encuestas.create({
-            'id_usuarios': usuario.id,
-            'fecha': fechaSismo,
-            'intensidad': intensidad.nombre
+        console.log(intensidadesObject);
 
-        }, {
-            fields: [
-                'id_usuarios',
-                'fecha',
-                'intensidad'
-            ], transaction: t
-        })
+        const total = intensidadesObject.reduce((a, b) => a + b.valor, 0);
 
-        for (respuesta of respuestas) {
-            let intensidad = await db.Intensidades.findOne({
-                where: {
-                    id_intensidad
-                }
-            })
-            await db.EncuestasDetalles.create({
-                'id_encuestas': encuestas.id,
-                'id_preguntas': fechaSismo,
-                'id_intensidades': intensidad.id
-
-            }, {
-                fields: [
-                    'id_encuestas',
-                    'id_preguntas',
-                    'id_intensidades'
-                ], transaction: t
-            })
-        }
-
-        //await usuario.setEncuestasDetalles(roles, { fields: ["id_encuestas", "id_preguntas","id_intesidades"], transaction: t });
-        await t.commit();*/
+        console.log(total);
+        /* const numero = await respuestas.length;
+         const intensidadValor = Math.round(intensidadesSuma/numero);
+         const intensidad = await db.Intensidades.findAll(
+             { where: { 'valor': intensidadValor } }
+         )
+         let fechaSismo = await fecha + ' ' + hora;
+         console.log(intensidad);
+         fechaSismo = dateFormat(fechaSismo, "yyyy-mm-dd h:MM:ss");
+         const encuestas = await db.Encuestas.create({
+             'id_usuarios': usuario.id,
+             'fecha': fechaSismo,
+             'intensidad': intensidad.nombre
+ 
+         }, {
+             fields: [
+                 'id_usuarios',
+                 'fecha',
+                 'intensidad'
+             ], transaction: t
+         })
+ 
+         for (respuesta of respuestas) {
+             let intensidad = await db.Intensidades.findOne({
+                 where: {
+                     id_intensidad
+                 }
+             })
+             await db.EncuestasDetalles.create({
+                 'id_encuestas': encuestas.id,
+                 'id_preguntas': fechaSismo,
+                 'id_intensidades': intensidad.id
+ 
+             }, {
+                 fields: [
+                     'id_encuestas',
+                     'id_preguntas',
+                     'id_intensidades'
+                 ], transaction: t
+             })
+         }
+ 
+         //await usuario.setEncuestasDetalles(roles, { fields: ["id_encuestas", "id_preguntas","id_intesidades"], transaction: t });
+         await t.commit();*/
         res.status(201).json({ mensaje: 'usuario creado' });
     } catch (e) {
         console.log(e);
