356c309ca87b3b92a09767c173cceeae6d39600e
Yamil Orozco <yamiloro@gmail.com>
2021-03-27 20:47:48 -0400

    agregando el cors

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === 'production') {
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
     });
-}/*
+}*/
 
 
 
@@ -41,6 +41,15 @@ if (process.env.NODE_ENV === 'production') {
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
     next();
 });*/
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, PATCH, OPTIONS, PUT, DELETE');
+    next();
+});
+
 
 const dir = path.join(__dirname, 'uploads');
 
