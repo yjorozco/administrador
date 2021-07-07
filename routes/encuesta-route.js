f570ec93607938f581ccf34a921dbd832bbbf5f0
Yamil Orozco <yamiloro@gmail.com>
2021-07-07 19:21:25 -0400

    arreglando el route de consultar encuesta

--- routes/encuesta-route.js
+++ routes/encuesta-route.js
@@ -7,7 +7,7 @@ const ACL = require('../middleware/acl');
 
 
 router.post('/', ACL('usuario_procesar_encuestas'), [check('ip').not().isEmpty().trim().escape(), check('longitud').not().isEmpty().trim().escape(), check('latitud').not().isEmpty().trim().escape(),check('respuestas').not().isEmpty()], procesarEncuesta);
-router.get('/:offset/:limit', ACL('usuario_ver_encuestas'), consultarEncuesta);
+router.get('/obtener/:offset/:limit', ACL('usuario_ver_encuestas'), consultarEncuesta);
 router.get('/:fecha_inicial/:fecha_final', ACL('admin_consultar_encuestas'), consultarEncuestaPorFecha);
 
 module.exports = router;
